07b27b1dcadb24bff7155c88c35b4ac18ffd1718
Nikan Zeyaei <nikanzeyaei@gmail.com>
2022-02-17 20:13:15 +0330

    Removed unnecessary stuff

--- src/app/helpers/name.ts
+++
@@ -1,7 +0,0 @@
-import { User } from "grammy/out/platform";
-
-export function getFullName(from: User) {
-  return from.last_name
-    ? `${from.first_name} ${from.last_name}`
-    : from.first_name;
-}
